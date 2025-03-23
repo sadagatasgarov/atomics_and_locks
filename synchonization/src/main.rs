c1ff61628dced26a5bb9a0999e7ccaef810a596f
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-03-24 01:04:35 +0400

    34 sync

--- synchonization/src/main.rs
+++ synchonization/src/main.rs
@@ -1,3 +1,30 @@
+use std::{sync::atomic::AtomicUsize, thread, time::Duration};
+
 fn main() {
-    
+    let num_done = AtomicUsize::new(0);
+    let main_mainthread = thread::current();
+
+    thread::scope(|s| {
+        s.spawn(||{
+            for i in 0..100 {
+                println!("{}", i);
+                num_done.store(i+1, std::sync::atomic::Ordering::Relaxed);
+                main_mainthread.unpark();
+            }
+        });
+
+        loop {
+            let n = num_done.load(std::sync::atomic::Ordering::Relaxed);
+            if n == 100 { break;};
+            println!("Working.. {n}/100 done");
+            thread::park_timeout(Duration::from_secs(1));
+        }
+
+
+    });
+
+    println!("Done!")
+
+
+
 }
