3b5acdc45cf1b582fe3dc76b2e95e7f3c44a4cb4
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-03-25 12:54:45 +0400

    gecikmeni gordum ela

--- statistics/src/main.rs
+++ statistics/src/main.rs
@@ -4,47 +4,55 @@ use std::{
     time::{Duration, Instant},
 };
 
+use std::sync::atomic::Ordering::Relaxed;
+
 fn main() {
     let num_done = &AtomicUsize::new(0);
     let total_time = &AtomicU64::new(0);
     let max_time = &AtomicU64::new(0);
 
     thread::scope(|s| {
+
         for t in 0..4 {
             s.spawn(move || {
                 for i in 0..25 {
                     let start = Instant::now();
-                    println!("{}", t * 25 + i);
+                    process_item(t * 25 + i);
                     let time_taken = start.elapsed().as_micros() as u64;
-                    num_done.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-                    total_time.fetch_add(time_taken, std::sync::atomic::Ordering::Relaxed);
-                    max_time.fetch_max(time_taken, std::sync::atomic::Ordering::Relaxed);
+                    num_done.fetch_add(1, Relaxed);
+                    total_time.fetch_add(time_taken, Relaxed);
+                    max_time.fetch_max(time_taken, Relaxed);
                 }
             });
+            thread::sleep(Duration::from_micros(500));
+        }
+
+        loop {
+            let total_time = Duration::from_micros(total_time.load(Relaxed));
+            let max_time = Duration::from_micros(max_time.load(Relaxed));
+            let n = num_done.load(Relaxed);
+    
+            if n == 100 {
+                break;
+            }
+
+            if n == 0 {
+                println!("working nothing done yet");
+            } else {
+                println!(
+                    "working.. {n}/100 done, {:?} avarage, {:?} peak",
+                    total_time / n as u32,
+                    max_time
+                );
+            }
+            thread::sleep(Duration::from_secs(1));
         }
     });
 
-    loop {
-        let total_time =
-            Duration::from_micros(total_time.load(std::sync::atomic::Ordering::Relaxed));
-        let max_time = Duration::from_micros(max_time.load(std::sync::atomic::Ordering::Relaxed));
+    println!("Done!");
+}
 
-        let n = num_done.load(std::sync::atomic::Ordering::Relaxed);
 
-        if n == 100 {
-            break;
-        }
-        if n == 0 {
-            println!("working nothing done yet")
-        } else {
-            println!(
-                "working.. {n}/100 done, {:?} avarage, {:?} peak",
-                total_time / n as u32,
-                max_time
-            );
-        }
-        thread::sleep(Duration::from_secs(1));
-    }
-
-    println!("Done!")
+fn process_item(_index: usize) {
+    thread::sleep(Duration::from_nanos(1));
 }
