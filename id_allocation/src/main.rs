23a3be6ade0d7e333bc2fd412b2b2bbd7cbf95ee
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-03-26 12:19:55 +0400

    compara an exchange

--- id_allocation/src/main.rs
+++ id_allocation/src/main.rs
@@ -2,13 +2,17 @@ use std::sync::atomic::Ordering::Relaxed;
 use std::sync::atomic::{AtomicI32, AtomicU32};
 
 fn main() {
+    let at = AtomicU32::new(2);
     allocate_new_id();
     allocate_new_id();
     let a = allocate_new_id();
     println!("{a}");
     allocate_new_id();
     let a = allocate_new_id();
-    println!("{a}")
+    println!("{a}");
+
+    increment(&at);
+    println!("{:?}", at);
 }
 
 // fn allocate_new_id() -> u32 {
@@ -25,4 +29,15 @@ fn allocate_new_id() -> u32 {
     }
 
     id
+}
+
+fn increment(a: &AtomicU32) {
+    let mut current = a.load(Relaxed);
+    loop {
+        let new = current + 1;
+        match a.compare_exchange(current, new, Relaxed, Relaxed) {
+            Ok(_) => return,
+            Err(v) => current = v,
+        }
+    }
 }
