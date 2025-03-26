7ca1b7f8e669119b0b44e174ab2e67ef096ac1e7
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-03-26 12:28:08 +0400

    id allocate without overflow

---
+++ id_allocation_without_overflow/src/main.rs
@@ -0,0 +1,19 @@
+use std::sync::atomic::AtomicU32;
+use std::sync::atomic::Ordering::Relaxed;
+fn main() {
+    println!("Hello, world!");
+}
+
+
+fn allocate_new_id() ->u32{
+    static NEXT_ID: AtomicU32= AtomicU32::new(0);
+    let mut id = NEXT_ID.load(Relaxed);
+
+    loop {
+        assert!(id<1000, "too many IDs!");
+        match NEXT_ID.compare_exchange_weak(id, id+1, Relaxed, Relaxed) {
+            Ok(_) => return id,
+            Err(v) => id = v
+        }
+    }
+}
