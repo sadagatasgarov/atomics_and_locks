370afb1d4c8ac1be5c17f882ed3c63ccbefe61e4
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-03-28 15:33:57 +0400

    daha tehlukesli bir vaianti

--- Release_and_Acure_ordering/src/main.rs
+++ Release_and_Acure_ordering/src/main.rs
@@ -2,21 +2,45 @@
 use std::{sync::atomic::{AtomicBool, AtomicU64, Ordering::{Acquire, Relaxed, Release}}, thread, time::Duration};
 
 
-static DATA: AtomicU64 = AtomicU64::new(0);
+// static DATA: AtomicU64 = AtomicU64::new(0);
+// static READY: AtomicBool = AtomicBool::new(false);
+
+
+// fn main() {
+//     thread::spawn(|| {
+//         DATA.store(123, Relaxed);
+//         READY.store(true, Release);
+//     });
+
+//     while !READY.load(Acquire)  {
+//         thread::sleep(Duration::from_millis(100));
+//         println!("waiting...")  
+//     }
+
+//     println!("{}", DATA.load(Relaxed));
+
+// }
+
+
+static mut DATA: u64 = 0;
 static READY: AtomicBool = AtomicBool::new(false);
 
 
 fn main() {
     thread::spawn(|| {
-        DATA.store(123, Relaxed);
+        unsafe  {
+            DATA = 123
+        };
+
         READY.store(true, Release);
     });
 
-    while !READY.load(Acquire)  {
-        thread::sleep(Duration::from_millis(100000));
-        println!("waiting...")  
+    while !READY.load(Acquire) {
+        thread::sleep(Duration::from_millis(100));
+        println!("waiting...");
     }
 
-    println!("{}", DATA.load(Relaxed));
-
+    println!("{}", unsafe {
+        DATA
+    });
 }
