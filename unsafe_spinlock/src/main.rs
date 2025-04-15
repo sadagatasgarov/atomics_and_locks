d8207ed0e8cd3ab484215c2e8fe907fb7ee6b387
Sadagat Asgarov <sadagat.asgarov@gmail.com>
2025-04-15 12:20:36 +0400

    unsafa_sinlovk

---
+++ unsafe_spinlock/src/main.rs
@@ -0,0 +1,36 @@
+use std::cell::UnsafeCell;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering::{Acquire, Release};
+
+pub struct SpinLock<T> {
+    locked: AtomicBool,
+    value: UnsafeCell<T>,
+}
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
+
+impl<T> SpinLock<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> Guard<T> {
+        while self.locked.swap(true, Acquire) {
+            std::hint::spin_loop();
+        }
+        Guard { lock: self }
+    }
+
+    pub unsafe fn unlock(&self) {
+        self.locked.store(false, Release);
+    }
+}
+
+pub struct Guard<'a, T> {
+    lock: &'a SpinLock<T>,
+}
+
+fn main() {}
